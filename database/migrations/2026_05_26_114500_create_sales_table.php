c492d7121ada58545deb0851ed553dc474c6e1d1
ilya <poctadladota@gmail.com>
2026-05-26 21:06:13 +0900

    create Sale model

---
+++ database/migrations/2026_05_26_114500_create_sales_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('g_number');
+            $table->date('date');
+            $table->date('last_change_date');
+            $table->string('supplier_article');
+            $table->string('tech_size')->nullable();
+            $table->bigInteger('barcode');
+            $table->decimal('total_price', 15, 2);
+            $table->integer('discount_percent');
+            $table->boolean('is_supply');
+            $table->boolean('is_realization');
+            $table->decimal('promo_code_discount', 15, 2)->nullable();
+            $table->string('warehouse_name');
+            $table->string('country_name');
+            $table->string('oblast_okrug_name');
+            $table->string('region_name');
+            $table->unsignedBigInteger('income_id');
+            $table->string('sale_id');
+            $table->unsignedBigInteger('odid')->nullable();
+            $table->decimal('spp', 5, 2);
+            $table->decimal('for_pay', 15, 2);
+            $table->decimal('finished_price', 15, 2);
+            $table->decimal('price_with_disc', 15, 2);
+            $table->bigInteger('nm_id');
+            $table->string('subject')->nullable();
+            $table->string('category')->nullable();
+            $table->string('brand')->nullable();
+            $table->boolean('is_storno')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
